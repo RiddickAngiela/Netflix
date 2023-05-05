d80988665fe27803fe8b579a83756cdcd49c8ee3
RiddickAngiela <angielariddick@gmail.com>
2023-05-05 12:49:52 +0300

    Styling main component

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -18,10 +18,12 @@ const Main = () => {
         <div className='w-full h-full'>
             <div className='absolute w-full h-[500px] bg-gradient-to-r from-black'></div>
             <img className='w-full h-full object-cover' src={`https://image.tmdb.org/t/p/original/${movie?.backdrop_path}`} alt={movie?.title} />
-            <div>
+           <div>
+             <div>
                 <button className='border bg-gray-300 text-black  border-gray-300 py-2 px-5'>Play</button>
                 <button className='border  text-white ml-5  border-gray-300 py-2 px-5'>Watch Later</button>
             </div>
+           </div>
         </div>
     </div>
   )
