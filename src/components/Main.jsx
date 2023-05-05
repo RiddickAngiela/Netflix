e984b8f32160fcd4ea3e27e46705a2ed4f21564b
RiddickAngiela <angielariddick@gmail.com>
2023-05-05 12:48:25 +0300

    Styling main component

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,17 +1,29 @@
 import React, { useEffect, useState } from 'react'
 import requests from '../Requests'
+import axios from 'axios'
 
 const Main = () => {
     const[movies, setMovies] = useState([])
 
+    const movie = movies[Math.floor(Math.random() * movies.length)];
+
     useEffect(()=>{
         axios.get(requests.requestPopular).then((response)=>{
-            setMovies(response.data)
+            setMovies(response.data.results)
         })
     },[])
     console.log(movies)
   return (
-    <div>Main</div>
+    <div className='w-full h-[500px] text-white'>
+        <div className='w-full h-full'>
+            <div className='absolute w-full h-[500px] bg-gradient-to-r from-black'></div>
+            <img className='w-full h-full object-cover' src={`https://image.tmdb.org/t/p/original/${movie?.backdrop_path}`} alt={movie?.title} />
+            <div>
+                <button className='border bg-gray-300 text-black  border-gray-300 py-2 px-5'>Play</button>
+                <button className='border  text-white ml-5  border-gray-300 py-2 px-5'>Watch Later</button>
+            </div>
+        </div>
+    </div>
   )
 }
 
