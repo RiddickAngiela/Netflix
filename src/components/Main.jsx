e78faca1d2cbb58b225001a429221784d8e0a280
RiddickAngiela <angielariddick@gmail.com>
2023-05-05 12:55:21 +0300

    adding title

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -18,7 +18,8 @@ const Main = () => {
         <div className='w-full h-full'>
             <div className='absolute w-full h-[500px] bg-gradient-to-r from-black'></div>
             <img className='w-full h-full object-cover' src={`https://image.tmdb.org/t/p/original/${movie?.backdrop_path}`} alt={movie?.title} />
-           <div>
+           <div className='absolute w-full top-[20%] p-4 md:p-8'>
+            <h1>{movie?.title}</h1>
              <div>
                 <button className='border bg-gray-300 text-black  border-gray-300 py-2 px-5'>Play</button>
                 <button className='border  text-white ml-5  border-gray-300 py-2 px-5'>Watch Later</button>
