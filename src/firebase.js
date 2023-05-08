6306a01fea9464f2224e1481fe51f3710108573a
RiddickAngiela <angielariddick@gmail.com>
2023-05-08 14:49:55 +0300

    initializing firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,17 +1,21 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAZ658W6K8jJ-iupayplIOQvZ9I6ph-vUI",
-  authDomain: "netflix-bc92c.firebaseapp.com",
-  projectId: "netflix-bc92c",
-  storageBucket: "netflix-bc92c.appspot.com",
-  messagingSenderId: "323983792483",
-  appId: "1:323983792483:web:6a3bb4a1e1d77865605b56",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain:process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId:process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket:process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId:process.env.REACT_APP_MESSAGING_SENDER ,
+  appId:process.env.REACT_APP_APP_ID ,
 };
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
