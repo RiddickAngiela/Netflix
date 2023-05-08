5193c08238e9be31d04081e5cfcd3f25eb149d71
RiddickAngiela <angielariddick@gmail.com>
2023-05-08 13:52:51 +0300

    Scrollbar

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -4,7 +4,7 @@ import React, { useEffect, useState } from 'react'
 import Movie from './Movie'
 
 
-const Row = ({title, fetchURL}) => {
+const Row = ({title, fetchURL, rowID}) => {
     const[movies,setMovies]= useState([])
     
 
