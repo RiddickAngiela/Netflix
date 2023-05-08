6bc60ffb9f352e3e9f2609931bb8e14e5f9066e1
RiddickAngiela <angielariddick@gmail.com>
2023-05-08 14:14:45 +0300

    Firebase setup

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAZ658W6K8jJ-iupayplIOQvZ9I6ph-vUI",
+  authDomain: "netflix-bc92c.firebaseapp.com",
+  projectId: "netflix-bc92c",
+  storageBucket: "netflix-bc92c.appspot.com",
+  messagingSenderId: "323983792483",
+  appId: "1:323983792483:web:6a3bb4a1e1d77865605b56",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
