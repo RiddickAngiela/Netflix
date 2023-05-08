bf8a93b4d14ec6b9c62756ba6a1655eed66ab1f0
RiddickAngiela <angielariddick@gmail.com>
2023-05-08 13:40:29 +0300

    Slider

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -1,5 +1,5 @@
 import axios from 'axios'
-
+import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
 import React, { useEffect, useState } from 'react'
 import Movie from './Movie'
 
@@ -14,16 +14,36 @@ const Row = ({title, fetchURL}) => {
       })
     },[fetchURL])
 
-      console.log(movies)
+     const slideLeft = () => {
+    var slider = document.getElementById('slider' + rowID);
+    slider.scrollLeft = slider.scrollLeft - 500;
+  };
+  const slideRight = () => {
+    var slider = document.getElementById('slider' + rowID);
+    slider.scrollLeft = slider.scrollLeft + 500;
+  };
+
+      // console.log(movies)
   return (
     <>
         <h2 className='text-white font-bold md:text-xl p-4'>{title}</h2>
         <div className='relative flex items-center'>
-            <div id={'slider'}>
+            <MdChevronLeft
+          onClick={slideLeft}
+          className='bg-white left-0 rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block'
+          size={40}
+        />
+
+            <div id={'slider'} className='w-full h-full overflow-x-scroll whitespace-nowrap scroll-smooth scrollbar-hide relative'>
             {movies.map((item,id)=>(
                 <Movie key={id} item={item}/>
             ))}
             </div>
+            <MdChevronRight
+          onClick={slideRight}
+          className='bg-white right-0 rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block'
+          size={40}
+        />
         </div>
     </>
   );
