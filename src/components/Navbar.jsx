a5a7f590ede9ac38fb4140927872f9c27791dc46
RiddickAngiela <angielariddick@gmail.com>
2023-05-09 13:47:58 +0300

    Modifying navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,35 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { UserAuth } from '../context/AuthContext'
 
 const Navbar = () => {
+   const {user, logOut} = UserAuth()
+  // console.log(user)
+
+    const handleLogout = async () => {
+      try{
+        await logOut();
+      }catch(error){
+        console.log(error);
+      }
+    }
+
   return (
     <div className='flex items-center justify-between p-4 z-(100) w-full absolute'>
       <Link to='/'>
          <h1 className='text-red-600 text-4xl font-bold cursor-pointer'>NETFLIX</h1>
       </Link>
        
-        <div className=''>
+       {user?.email ?  <div className=''>
+          <Link to='/account'>
+               <button className='text-white pr-4'>Account</button>
+          </Link>
+           <Link to='/signup'>
+               <button className='bg-red-600 px-6 py-2 rounded cursor-pointer text-white'>Logout</button>
+           </Link>
+           
+        </div> :
+         <div className=''>
           <Link to='/login'>
                <button className='text-white pr-4'>Sign In</button>
           </Link>
@@ -16,7 +37,7 @@ const Navbar = () => {
                <button className='bg-red-600 px-6 py-2 rounded cursor-pointer text-white'>Sign Up</button>
            </Link>
            
-        </div>
+        </div>}
     </div>
   )
 }
