98dbf23db4113caa05e26e0f947bb49cd94e31c8
RiddickAngiela <angielariddick@gmail.com>
2023-05-09 17:25:18 +0300

    Saved shows

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -5,7 +5,7 @@ import { UserAuth } from '../context/AuthContext';
 const Login = () => {
     const[email,setEmail] = useState('')
     const[password,setPassword] = useState('')
-     const[user, logIn] = UserAuth()
+     const[logIn] = UserAuth()
      const[error, setError] = useState('')
     const navigate = useNavigate()
 
@@ -24,7 +24,7 @@ const Login = () => {
 
   return (
     <div className='w-full h-screen'> 
-        <img className='hidden sm:block absolute w-full object-cover' src='https://www.notebookcheck.net/fileadmin/Notebooks/News/_nc3/netflixteaser.png' alt='/' />
+        <img className='hidden sm:block absolute w-full object-cover' src='https://assets.nflxext.com/ffe/siteui/vlv3/f841d4c7-10e1-40af-bcae-07a3f8dc141a/f6d7434e-d6de-4185-a6d4-c77a2d08737b/US-en-20220502-popsignuptwoweeks-perspective_alpha_website_medium.jpg' alt='/' />
         <div className='bg-black/80 fixed top-0 left-0 w-full h-screen'></div>
         <div className='fixed w-full px-4 py-24 z-50'>
             <div className='max-w-[450px] h-[600px] mx-auto bg-black/75 text-white'>
