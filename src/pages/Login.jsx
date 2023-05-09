b0a30f28bce69f37e25e2243250b128b397453a8
RiddickAngiela <angielariddick@gmail.com>
2023-05-09 13:26:51 +0300

    Modifying LOgin

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -9,18 +9,18 @@ const Login = () => {
         <div className='fixed w-full px-4 py-24 z-50'>
             <div className='max-w-[450px] h-[600px] mx-auto bg-black/75 text-white'>
                 <div className='max-w-[320px] mx-auto py-16'>
-                    <h1 className='text-3xl font-bold'>Sign Up</h1>
+                    <h1 className='text-3xl font-bold'>Sign In</h1>
                     <form className='w-full flex flex-col py-4'>
                         <input className='p-3 my-2 bg-grey-700 rounded' type="email" placeholder='Email' autoComplete='email'/>
                         <input className='p-3 my-2 bg-grey-700 rounded' type='password' placeholder='Password' autoComplete='current-password'/>
-                        <button className='bg-red-600 py-3 my-6 rounded font-bold'>Sign Up</button>
+                        <button className='bg-red-600 py-3 my-6 rounded font-bold'>Sign In</button>
                         <div className='flex justify-between items-center text-sm text-grey-600'>
                             <p><input className='mr-2' type='checkbox'/>Remember me</p>
                             <p>Need Help?</p>
                         </div>
-                        <p className='py-8'><span className='text-grey-600'>Already subscribe to Netflix?</span>
-                        <Link to='/login'>
-                             Sign In
+                        <p className='py-8'><span className='text-grey-600'>New to Netflix?</span>{' '}
+                        <Link to='/signup'>
+                             Sign Up
                         </Link>
                        </p>
                     </form>
