a502151cb7c650839cdc8311e08568913d475dbe
RiddickAngiela <angielariddick@gmail.com>
2023-05-09 12:43:13 +0300

    SignUp

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -13,7 +13,10 @@ const Signup = () => {
                         <input className='p-3 my-2 bg-grey-700 rounded' type="email" placeholder='Email' autoComplete='email'/>
                         <input className='p-3 my-2 bg-grey-700 rounded' type='password' placeholder='Password' autoComplete='current-password'/>
                         <button className='bg-red-600 py-3 my-6 rounded font-bold'>Sign Up</button>
-                        
+                        <div className='flex justify-between items-center'>
+                            <p><input className='mr-2' type='checkbox'/>Remember me</p>
+                            <p>Need Help?</p>
+                        </div>
                     </form>
                 </div>
             </div>
